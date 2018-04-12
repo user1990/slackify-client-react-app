d6c58fce6aa2d6c0575f08c256945d6a605d785e
user1990 <paulius.rimg1990@gmail.com>
2018-04-12 09:17:49 +0300

    Semantic UI React Comment Component

--- src/components/ViewTeams/Messages.js
+++ src/components/ViewTeams/Messages.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 import { graphql } from 'react-apollo';
+import { Comment } from 'semantic-ui-react';
 import { messagesQuery } from '../../graphql/queries/queries';
 
 const Messages = ({ data: { loading, messages } }) =>
-  loading ? null : <div>{JSON.stringify(messages)}</div>;
+  loading ? null : (
+    <Comment.Group className="comments">
+      {messages.map(message => (
+        <Comment key={`${message.id}-message`}>
+          <Comment.Content>
+            <Comment.Author as="a">{message.user.username}</Comment.Author>
+            <Comment.Metadata>
+              <div>{message.created_at}</div>
+            </Comment.Metadata>
+            <Comment.Text>{message.text}</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      ))}
+    </Comment.Group>
+  );
 
 export default graphql(messagesQuery, {
   variables: props => ({
--- src/graphql/queries/queries.js
+++ src/graphql/queries/queries.js
@@ -31,7 +31,7 @@ export const messagesQuery = gql`
       user {
         username
       }
-      createdAt
+      created_at
     }
   }
 `;
